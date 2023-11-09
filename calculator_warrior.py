75229824a8c2eb4acb3587ffc530e0b7191c4fb4
Valerii <valeraolen094@gmail.com>
2023-11-09 18:16:29 +0100

    Remade basic code and deleted button_1

--- calculator_warrior.py
+++ calculator_warrior.py
@@ -1,21 +1,18 @@
 import pygame
-import sys
-from button_1 import Button_1
 
-def run():
-    pygame.init()
-    screen = pygame.display.set_mode((302, 516))
-    pygame.display.set_caption("Calculator warrior")
-    bg_image = pygame.image.load("images\BG\BG.png")
-    button_1 = Button_1(screen)
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+pygame.init()
+screen = pygame.display.set_mode((302, 516))
+pygame.display.set_caption("Calculator warrior")
+bg_image = pygame.image.load("images\BG\BG.png")
+
+running = True
+
+while running:
     
-        screen.blit(bg_image, [0,0])
-        button_1.output()
-        pygame.display.flip()
+    pygame.display.update()
 
-run()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
